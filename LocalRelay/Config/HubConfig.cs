8ae7c53fcb12282a7fcdfc167543418b9cc573b4
LucHeart <luc@luc.cat>
2025-04-22 02:21:42 +0200

    auto connect

--- LocalRelay/Config/HubConfig.cs
+++ LocalRelay/Config/HubConfig.cs
@@ -3,5 +3,4 @@
 public sealed class HubConfig
 {
     public Guid? Hub { get; set; } = null;
-    public bool AutoConnect { get; set; } = true;
 }
--- LocalRelay/Config/SerialConfig.cs
+++ LocalRelay/Config/SerialConfig.cs
@@ -2,7 +2,6 @@
 
 public sealed class SerialConfig
 {
-    public bool AutoSelect { get; set; } = true;
     public bool AutoConnect { get; set; } = true;
     public string? Port { get; set; } = null;
 }
